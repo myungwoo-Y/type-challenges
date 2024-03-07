89b05478e4e6bb1f9bf61457e334d766490f26f6
myungwoo-Y <auddn6676@naver.com>
2024-03-08 02:15:46 +0900

    solve 4 - easy pick

--- questions/00004-easy-pick/test-cases.ts
+++ questions/00004-easy-pick/test-cases.ts
@@ -1,5 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+
+type a = MyPick<Todo, 'title'>
 type cases = [
   Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
   Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
