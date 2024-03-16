6d6d6fd1045d0c4a393d15e11ae52cd752f442f1
myungwoo-Y <auddn6676@naver.com>
2024-03-16 23:49:08 +0900

    solve 898 - easy includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,8 @@
-type Includes<T extends readonly any[], U> = any
+type Equal<T, U> = (<G>() => G extends T ? 1 : 2) extends
+(<G>() => G extends U ? 1 : 2) ? true : false
+
+type Includes<T extends readonly unknown[], U> = T extends [infer First, ...infer Last]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Last, U>
+  : false
